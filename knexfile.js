6acd81e3239db22ed6809f90efcea19857876bb3
João Bruno <j_bruno_s@hotmail.com>
2020-04-08 19:18:14 -0300

    altera variáveis de ambiente

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: './src/database/migrations'
