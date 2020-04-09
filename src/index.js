89e04e74d25a567b3c4f7d8df130b0dfb80ec63a
João Bruno <j_bruno_s@hotmail.com>
2020-04-09 16:58:31 -0300

    altera informações de acesso do CORS

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 const app = express();
 
-app.use(cors( 'https://gerenciador-aluguel-front.herokuapp.com/' ))
+app.use(cors())
 app.use(express.json())
 app.use(routes);
 
