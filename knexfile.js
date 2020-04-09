2c1e8271588076e8a11abdc9d5c413c361565bf9
João Bruno <j_bruno_s@hotmail.com>
2020-04-09 15:45:55 -0300

    adiciona caminho para acessar as migrates

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,9 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    
+    migrations: {
+      directory: './src/database/migrations'
+    }
   }
 
 };
